c358936c48bbd765ed0edb1c4ffd20d7aebf3dff
Ridwyn <salihouridwyn@gmail.com>
2021-02-17 19:09:11 +0000

    heroku

--- models/Status_template.js
+++ models/Status_template.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
-const statusSchema = require(__appRoot+'/models/Status.js').schema
+// const statusSchema = require(__appRoot+'/models/Status.js').schema
 const Schema = mongoose.Schema
 
 const statusTemplateSchema = new Schema({
   name:String,
-  statuses:[statusSchema]
+  // statuses:[statusSchema]
 })
 
 module.exports = mongoose.model('Status_template', statusTemplateSchema)
