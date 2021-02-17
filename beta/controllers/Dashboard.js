6d76483c225ed3bd0afadeee71f522d85cbf886c
Ridwyn <salihouridwyn@gmail.com>
2021-02-17 19:15:49 +0000

    heroku

--- beta/controllers/Dashboard.js
+++ beta/controllers/Dashboard.js
@@ -6,8 +6,8 @@ let Workspace = require(__appRoot+'/models/Workspace.js')
 let Project = require(__appRoot+'/models/Project.js')
 let Task = require(__appRoot+'/models/Task.js')
 let User = require(__appRoot+'/models/User.js')
-let Status = require(__appRoot+'/models/Status.js')
-let DStatus = require(__appRoot+'/models/Default_status.js')
+// let Status = require(__appRoot+'/models/Status.js')
+// let DStatus = require(__appRoot+'/models/Default_status.js')
 
 
 dashboardController.get('/:space_id?:project_id?',async (req,res)=>{
